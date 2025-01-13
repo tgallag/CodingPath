df7698e1a3042bcbb18d459101fdce4662ed68c5
tgallag <timjgallagher02@gmail.com>
2025-01-13 11:30:30 -0500

    gg chat

---
+++ two_pointer.py
@@ -0,0 +1,19 @@
+palindrome = "hannah"
+name = palindrome
+
+def two_pointer(name):
+    left = 0
+    right = len(name) - 1
+    while left <= right:
+        if name[left] == name[right]:
+            print(name[left])
+            left = left + 1
+            right = right - 1
+        else:
+            print(name[left])
+            return False
+    return True
+
+print(two_pointer(palindrome))
+         
+        
